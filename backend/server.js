583ab4dfbd6799bfdb8d941e932f538ad979d116
Ujiro Asagbra <ujiroasagbra@gmail.com>
2025-01-21 07:24:43 +0000

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ import dotenv from "dotenv";
 import userRoute from './routes/user.route.js'
 import entryRoute from './routes/journal.route.js'
 import cors from 'cors'
+import path from 'path'
 
 
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 app.use(cors())
 
@@ -18,6 +20,15 @@ app.use(express.json())
 
 app.use("/api/entry", entryRoute)
 app.use("/api/users", userRoute)
+
+if(process.env.NODE_ENV ==="production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+  app.get("*", (req,res)=> {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+
+  })
+}
 
 
 
